6e8efc9a4d25793723c974b417e2acfba6eff354
karolyim <matej.karolyi@gmail.com>
2021-05-10 20:13:06 +0200

    [MAINTENANCE] project init + assignment

---
+++ README.md
@@ -0,0 +1,11 @@
+# README #
+
+Czechitas Javascript 1 - Projekt zadaný na Lekci 10. Aplikace pro přehlednou kategorizaci receptů.
+
+Zadání
+---
+
+- Uprav si recepty v `recepty.js` podle sebe. Přidej své oblíbené, doplň popisy a další informace.
+- Zbytek zadání projektu je dostupný v souboru `app.js`.
+- Během práce můžeš libovolně upravovat CSS, HTML i JS, přejmenovávat soubory, vytvářet nové a nepotřebné mazat. Je to čistě na tobě. :-)
+- Svoji práci vhodně strukturuj a efektivně využívej Git.
---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+Co je za úkol v tomto projektu:
+
+1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
+HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
+
+2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
+by se měl seznam receptů vyfiltrovat podle hledaného slova.
+
+3) Doplň filtrovanání receptů podle kategorie.
+
+4) Doplň řazení receptů podle hodnocení.
+
+5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
+Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
+recept-hodnoceni, recept-nazev, recept-popis.
+
+6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
+*/
